792bab93a0d58893bb8379a310cd811430636779
dejunqi2008 <dejunqi2008@gmail.com>
2015-12-28 02:32:57 -0600

    blog page added

--- js/works.js
+++ js/works.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
 function loadLeetCode() {
 
-	var nums = [127, 98, 94, 73, 73, 69, 58, 28, 27, 26, 24, 22, 21, 20, 19, 17, 16, 15, 14, 11, 1];
+	var nums = [127, 98, 94, 73, 70, 69, 58, 28, 27, 26, 24, 22, 21, 20, 19, 17, 16, 15, 14, 11, 1];
 	var target = document.getElementById("solutionPDF");
 	for(var i = 0; i<nums.length; i++) {
 		var node = document.createElement('li');
